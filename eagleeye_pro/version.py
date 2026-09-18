670f7e10ff69fce9e511e4846931033e5b35a6d8
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-18 18:44:03 +0200

    Build 420: bump runtime version

--- eagleeye_pro/version.py
+++ eagleeye_pro/version.py
@@ -1,3 +1,3 @@
-BUILD = "419.0"
-SCHEMA_VERSION = "419.0"
-BUILD_NAME = "EagleEye Intelligence Platform – Build 419.0 Investigation Synthesis & Argumentation"
+BUILD = "420.0"
+SCHEMA_VERSION = "420.0"
+BUILD_NAME = "EagleEye Intelligence Platform – Build 420.0 Phase 18 Qualification"
