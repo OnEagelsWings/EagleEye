9d520c9845b6e941f4a88ed5d947792bdf7506dc
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-18 18:30:08 +0200

    Build 418: switch current server

--- src/eagleeye/interfaces/web/server.py
+++ src/eagleeye/interfaces/web/server.py
@@ -20,7 +20,7 @@ import uvicorn
 from eagleeye.bootstrap.runtime_lock import workspace_runtime_lock
 from eagleeye_pro.version import BUILD
 
-from .app417 import create_workspace_app417 as create_workspace_app
+from .app418 import create_workspace_app418 as create_workspace_app
 
 
 def _free_port(host: str, preferred: int) -> int:
