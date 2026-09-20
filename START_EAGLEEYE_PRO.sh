4f5068b021564019e87b93e5662804ee32738db3
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-20 13:20:46 +0200

    Build 424: advance runtime and launcher

--- START_EAGLEEYE_PRO.sh
+++ START_EAGLEEYE_PRO.sh
@@ -45,4 +45,4 @@ if ! "$VENV_PY" -c 'import fastapi, uvicorn, sqlalchemy, pydantic' >/dev/null 2>
   "$VENV_PY" -m pip install -e .
 fi
 
-exec "$VENV_PY" EAGLEEYE_PRO_423_0.py
+exec "$VENV_PY" EAGLEEYE_PRO_424_0.py
