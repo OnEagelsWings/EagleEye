9e23172601e68d570a4e0904fad567ef9ad5ba7e
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-20 13:20:44 +0200

    Build 424: advance runtime and launcher

--- src/eagleeye/interfaces/web/server.py
+++ src/eagleeye/interfaces/web/server.py
@@ -20,7 +20,7 @@ import uvicorn
 from eagleeye.bootstrap.runtime_lock import workspace_runtime_lock
 from eagleeye_pro.version import BUILD
 
-from .app423 import create_workspace_app423 as create_workspace_app
+from .app424 import create_workspace_app424 as create_workspace_app
 
 
 def _free_port(host: str, preferred: int) -> int:
