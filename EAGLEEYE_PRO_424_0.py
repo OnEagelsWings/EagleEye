134cc33494fe122622a2ff5716f24ca0a630f5b0
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-20 13:20:20 +0200

    Build 424: source health rate limits and freshness

---
+++ EAGLEEYE_PRO_424_0.py
@@ -0,0 +1,5 @@
+from eagleeye.interfaces.web.app424 import create_workspace_app424
+app=create_workspace_app424()
+if __name__=='__main__':
+ import uvicorn
+ uvicorn.run(app,host='127.0.0.1',port=8000)
