a6c79d00c51df21aab99bc884665664d749602ee
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-22 10:39:17 +0200

    Build 428: advance runtime and launchers

--- eagleeye_pro/version.py
+++ eagleeye_pro/version.py
@@ -1,3 +1,3 @@
-BUILD = "427.0"
-SCHEMA_VERSION = "427.0"
-BUILD_NAME = "EagleEye Intelligence Platform – Build 427.0 Incremental Crawling & Change Detection"
+BUILD = "428.0"
+SCHEMA_VERSION = "428.0"
+BUILD_NAME = "EagleEye Intelligence Platform – Build 428.0 Archives & Historical Web"
