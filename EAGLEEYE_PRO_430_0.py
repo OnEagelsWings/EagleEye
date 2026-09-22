9d5c23b3657bc8b01894f796ba2ab98de31d4b8c
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-22 12:49:33 +0200

    Checkpoint 430: use canonical workspace launcher

--- EAGLEEYE_PRO_430_0.py
+++ EAGLEEYE_PRO_430_0.py
@@ -1,5 +1,5 @@
 from eagleeye.interfaces.web.app430 import create_workspace_app430
+from eagleeye.interfaces.web.server import serve_workspace
 app=create_workspace_app430()
 if __name__=='__main__':
- import uvicorn
- uvicorn.run(app,host='127.0.0.1',port=8000)
+ raise SystemExit(serve_workspace())
