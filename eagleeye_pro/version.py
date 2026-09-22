6732048b9b6fef84ffb606fdd58130f77f536e2a
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-22 10:22:50 +0200

    Build 427: advance runtime and launchers

--- eagleeye_pro/version.py
+++ eagleeye_pro/version.py
@@ -1,3 +1,3 @@
-BUILD = "426.0"
-SCHEMA_VERSION = "426.0"
-BUILD_NAME = "EagleEye Intelligence Platform – Build 426.0 Crawl Prioritization & Budgets"
+BUILD = "427.0"
+SCHEMA_VERSION = "427.0"
+BUILD_NAME = "EagleEye Intelligence Platform – Build 427.0 Incremental Crawling & Change Detection"
