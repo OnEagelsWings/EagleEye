b5d046f3f53690dea25057b542a91eb8c143e239
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-22 10:54:14 +0200

    Build 430: advance runtime and launchers

--- eagleeye_pro/version.py
+++ eagleeye_pro/version.py
@@ -1,3 +1,3 @@
-BUILD = "429.0"
-SCHEMA_VERSION = "429.0"
-BUILD_NAME = "EagleEye Intelligence Platform – Build 429.0 News Connector Layer"
+BUILD = "430.0"
+SCHEMA_VERSION = "430.0"
+BUILD_NAME = "EagleEye Intelligence Platform – Build 430.0 News Entity/Event Extraction"
