da896c3be99cecf249e48619e07a1d15dba9b755
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-26 11:47:06 +0200

    Build 440: update Unix launcher

--- START_EAGLEEYE_PRO.sh
+++ START_EAGLEEYE_PRO.sh
@@ -45,4 +45,4 @@ if ! "$VENV_PY" -c 'import fastapi, uvicorn, sqlalchemy, pydantic' >/dev/null 2>
   "$VENV_PY" -m pip install -e .
 fi
 
-exec "$VENV_PY" EAGLEEYE_PRO_439_0.py
+exec "$VENV_PY" EAGLEEYE_PRO_440_0.py
