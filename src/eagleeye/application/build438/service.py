7a4dc94a62600048de82b371a6929fc050d0d300
OnEagelsWings <m.sprenger222@googlemail.com>
2026-09-26 06:24:55 +0200

    Build 439: keep Build 438 status coherent on newer runtime

--- src/eagleeye/application/build438/service.py
+++ src/eagleeye/application/build438/service.py
@@ -18,4 +18,4 @@ class Build438TemporalRelationshipFusionService:
     def run_temporal_relationship_case_selftest(self,**kw):return self.fusion438.run_case_selftest(**kw)
     def temporal_relationship_status_438(self):
         s=self.fusion438.status()
-        return {**s,'version_coherent':RUNTIME_BUILD==SCHEMA_VERSION==self.BUILD,'phase':19,'phase19_builds_completed':18,'production_release_ready':False}
+        return {**s,'version_coherent':RUNTIME_BUILD==SCHEMA_VERSION and int(RUNTIME_BUILD.split('.')[0])>=int(self.BUILD.split('.')[0]),'phase':19,'phase19_builds_completed':18,'production_release_ready':False}
